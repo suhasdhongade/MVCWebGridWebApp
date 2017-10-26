98306025d9c0752db39ac6540eced34e094e6dce
suhasdhongade <suhasdhongade@gmail.com>
2017-10-26 15:40:09 +0530

    Web grid web aapp by using Jquery Ajax

---
+++ MVCWebGridWebApp/Controllers/TestController.cs
@@ -0,0 +1,57 @@
+﻿using MVCWebGridWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebGridWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public static bool isAdded = false;
+
+        public ActionResult Index()
+        {
+            GridInfoSuhas grdDetails = new GridInfoSuhas();
+            grdDetails.ListOfCustomer = new List<RowModel>();
+            List<RowModel> lstCustomers = new List<RowModel>();
+            RowModel rowInfo;
+            for (int i = 1; i <= 15; i++)
+            {
+                rowInfo = new RowModel();
+                rowInfo.asdasd = "asdasd" + i;
+                rowInfo.asd = "asd" + i;
+                rowInfo.asddd = "asddd" + i;
+                lstCustomers.Add(rowInfo);
+            }
+            grdDetails.ListOfCustomer = lstCustomers;
+            return View(grdDetails);
+        }
+        [HttpGet]
+        public JsonResult SaveRecord(string asdasd, string asd, string asddd)
+        {
+            isAdded = true;
+            var result = true; return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult UpdateRecord(string asdasd, string asd, string asddd)
+        {
+            bool result = true;
+            try { }
+            catch (Exception ex) { }
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult DeleteRecord(string id)
+        {
+            bool result = true;
+            try { }
+            catch (Exception ex) { }
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MVCWebGridWebApp/Models/GridInfoSuhas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebGridWebApp.Models
+{
+    public class RowModel
+    {
+        public string asdasd { get; set; }
+        public string asd { get; set; }
+        public string asddd { get; set; }
+
+    }
+    public class GridInfoSuhas
+    {
+        public List<RowModel> ListOfCustomer { get; set; }
+
+    }
+}
